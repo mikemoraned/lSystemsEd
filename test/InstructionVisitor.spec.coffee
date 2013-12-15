8d2c79b9d0af5a845449abf0eb2e0e00aa14f276
Mike Moran <mike@houseofmoran.com>
2013-12-15 20:21:22 +0000

    minimal first test

--- test/InstructionVisitor.spec.coffee
+++ test/InstructionVisitor.spec.coffee
@@ -1,13 +1,19 @@
 describe('InstructionVisitor', () =>
   describe('basic', () =>
 
-    origin = new Vec2(0.0, -1.0)
+    origin = new Vec2(0.0, 0.0)
 
     describe('visitForward', () =>
-      it('single forward moves one unit in default direction', () =>
+      it('forward moves one unit in default direction', () =>
         parent = new Root(origin)
+        assert.isNull(parent.nextLink)
         visitor = new InstructionVisitor(origin, parent)
-        assert.equal(false, true, "to be completed")
+        visitor.visitForward('name','blue',1.0)
+        assert.isNotNull(parent.nextLink)
+        assert.equal(parent.nextLink.startParticle.pos.x, origin.x)
+        assert.equal(parent.nextLink.startParticle.pos.y, origin.y)
+        assert.equal(parent.nextLink.endParticle().pos.x, origin.x)
+        assert.equal(parent.nextLink.endParticle().pos.y, origin.y - 20.0)
       )
     )
   )
--- test/InstructionVisitor.spec.js
+++ test/InstructionVisitor.spec.js
@@ -5,13 +5,19 @@
   describe('InstructionVisitor', function() {
     return describe('basic', function() {
       var origin;
-      origin = new Vec2(0.0, -1.0);
+      origin = new Vec2(0.0, 0.0);
       return describe('visitForward', function() {
-        return it('single forward moves one unit in default direction', function() {
+        return it('forward moves one unit in default direction', function() {
           var parent, visitor;
           parent = new Root(origin);
+          assert.isNull(parent.nextLink);
           visitor = new InstructionVisitor(origin, parent);
-          return assert.equal(false, true, "to be completed");
+          visitor.visitForward('name', 'blue', 1.0);
+          assert.isNotNull(parent.nextLink);
+          assert.equal(parent.nextLink.startParticle.pos.x, origin.x);
+          assert.equal(parent.nextLink.startParticle.pos.y, origin.y);
+          assert.equal(parent.nextLink.endParticle().pos.x, origin.x);
+          return assert.equal(parent.nextLink.endParticle().pos.y, origin.y - 20.0);
         });
       });
     });
